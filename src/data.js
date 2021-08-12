faf6d3ffe9359f76d98a7b069838ad06016058df
Catarogu08 <cataguatame2963@gmail.com>
2021-08-12 10:26:29 -0500

    estilos,html

--- src/data.js
+++ src/data.js
@@ -1,3 +1,10 @@
+import data from "./data/pokemon/pokemon.js"
+
+console.log(data.pokemon)
+
+const arrayPokemones = data.pokemon;
+
+
 // estas funciones son de ejemplo
 
 export const example = () => {
