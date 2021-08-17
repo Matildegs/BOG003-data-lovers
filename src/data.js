adf2c0b7c699840d2f4c56d289390415fe5381be
Catarogu08 <cataguatame2963@gmail.com>
2021-08-17 12:05:45 -0500

    js

--- src/data.js
+++ src/data.js
@@ -1,11 +1,4 @@
-import data from "./data/pokemon/pokemon.js"
 
-console.log(data.pokemon)
-
-const arrayPokemones = data.pokemon;
-
-
-// estas funciones son de ejemplo
 
 export const example = () => {
   return 'example';
--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
 import data from './data/pokemon/pokemon.js';
 // importa la data de pokemones 
 
-console.log(example, data);
+const arrayPokemones= data.pokemon;
+const mostrarPokemones= () => {
+    const divPokemones= document.createElement("div");
+    arrayPokemones.forEach(poke=>{
+        const parrafoNombre= document.createElement("p");
+        parrafoNombre.innerHTML= poke.name;
+        console.log(parrafoNombre)
+        const divImage= document.createElement("img");
+        divImage.classList.add("imgPoke");
+        divImage.src= poke.img; 
+        console.log(divImage.src)
+        const divPokemon= document.createElement("div");
+        divPokemon.classList.add("tarjetaPokemon");
+        divPokemon.appendChild(parrafoNombre);
+        divPokemon.appendChild(divImage);
+        divPokemones.appendChild(divPokemon);
+        
+
+    }) 
+    document.querySelector(".container").appendChild(divPokemones);
+}
+window.addEventListener("Load", (mostrarPokemones()));
+
+
+// console.log(example, data);
